69460172a41955be64008c6e53012a08b5dc2f74
hyojKim2 <101076275+hyojKim2@users.noreply.github.com>
2024-05-21 22:40:49 +0900

    [level 2] Title: 의상, Time: 0.10 ms, Memory: 70.2 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -11,41 +11,14 @@ class Solution {
             multiMap.get(clothes[i][1]).add(clothes[i][0]);//의상 종류에 의상 이름 value 추가
         }
     
-        if(multiMap.size() == 30)
-            { return 1073741823; }
-        int answer = 0;
-        List<List<String>> keysList = getCombinedKeys(multiMap.keySet(), 1, multiMap.size());
+        int answer = 1;
+        
+        
+        for(String key: multiMap.keySet()){
+            answer*= (multiMap.get(key).size() +1) ;
+        }
 
-
-        for(List <String> keys : keysList){
-            int temp_answer=1;
-            for( String key : keys)
-                temp_answer*=  multiMap.get(key).size();
-            answer+=temp_answer;
-        } //인덱스 크기만큼 돌아서 해당하는 key곱하여 더함 
-            
-        return answer;
+        return answer-1;
     }
     
-    public static List<List<String>> getCombinedKeys(Set<String> keySet, int minLength, int maxLength) {
-        List<List<String>> resultList = new ArrayList<>();
-
-        // 최소 길이에서 최대 길이까지 조합을 생성
-        for (int len = minLength; len <= maxLength; len++) {
-            generateCombinations(resultList, new ArrayList<>(), keySet, len, 0);
-        }
-        return resultList;
-    }
-
-    private static void generateCombinations(List<List<String>> resultList, List<String> tempList, Set<String> keySet, int len, int start) {
-        if (tempList.size() == len) {
-            resultList.add(new ArrayList<>(tempList));
-            return;
-        }
-        for (int i = start; i < keySet.size(); i++) {
-            tempList.add((String) keySet.toArray()[i]);
-            generateCombinations(resultList, tempList, keySet, len, i + 1);
-            tempList.remove(tempList.size() - 1);
-        }
-    }
 }
