a4a25e5c105b170f250172e041c7399fd4a5bd17
hyojKim2 <101076275+hyojKim2@users.noreply.github.com>
2024-07-27 01:00:15 +0900

    [level 3] Title: 베스트앨범, Time: 3.68 ms, Memory: 81.6 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/README.md
+++ 프로그래머스/3/42579. 베스트앨범/README.md
@@ -1,10 +1,10 @@
 # [level 3] 베스트앨범 - 42579 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579#) 
 
 ### 성능 요약
 
-메모리: 75.6 MB, 시간: 1.64 ms
+메모리: 81.6 MB, 시간: 3.68 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 27일 00:49:33
+2024년 07월 27일 01:00:15
 
 ### 문제 설명
 
--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -1,112 +1,61 @@
 import java.util.*;
 
 class Solution {
-    /*
-    해시맵생성,
-    장르명: int[5] 배열
-    [0]: 장르의 총 재생수
-    [1]: 수록곡1의 플레이수
-    [2]: 수록곡1의 고유번호
-    [3]: 수록곡2의 플레이수
-    [4]: 수록곡2의 고유번호
-    반복문에서, 곡을 해당 장르의 수록곡1, 2와 재생횟수를 비교하여 배열 갱신
-
-    반복문종료: 최다재생수에 따라 고유번호 return
-
-
-    */
     public int[] solution(String[] genres, int[] plays) {
-        int[] answer ;
-
-        HashMap<String, Integer[]> map =new HashMap<>();
-
-        for(int i=0;i< genres.length; i++){
-
-            if(!map.containsKey(genres[i])){//맵에 장르가 없으면 추가해줌
-                Integer[] initialArray = new Integer[5];
-                Arrays.fill(initialArray, 0); // 배열을 0으로 초기화
-                map.put(genres[i], initialArray);
-
-                map.get(genres[i])[1]=plays[i]; //플레이수
-                map.get(genres[i])[2]=i; // 고유번호
-            }
-
-            //맵에 장르가 있고, 2번째 삽입인 경우
-            else if(map.get(genres[i])[3]==null){
-
-                map.get(genres[i])[3]=plays[i]; //플레이수
-                map.get(genres[i])[4]=i; // 고유번호
-            }
-
-            //맵에 장르가 있고, 3번째 이상 비교
-            else{
-                int minPlay=0; //두 곡 중 더 작은 플레이수와 인덱스를 저장
-                int minIdx=0;
-                if(map.get(genres[i])[1]>map.get(genres[i])[3]){
-                    minPlay=map.get(genres[i])[3];
-                    minIdx=3;
-                }
-                else{
-                    minPlay=map.get(genres[i])[1];
-                    minIdx=1;
-                }
-
-                if(plays[i]> minPlay){ //현재 플레이수와 비교해서, 현재 플레이수가 더 크다면 갱신
-                    map.get(genres[i])[minIdx]=plays[i];
-                    map.get(genres[i])[minIdx+1]=i;
-                }
-
-            }
-
-            map.get(genres[i])[0]+=plays[i]; //총 재생수 더해줌
-
-
-        }
-
-        List<Map.Entry<String, Integer[]>> list = new ArrayList<>(map.entrySet()); //키들을 리스트로 가져옴
-
+        // 장르별 정보를 저장할 해시맵
+        HashMap<String, GenreInfo> genreMap = new HashMap<>();
         
-          list.sort(new Comparator<Map.Entry<String, Integer[]>>() {
-            @Override
-            public int compare(Map.Entry<String, Integer[]> entry1, Map.Entry<String, Integer[]> entry2) {
-                // 내림차순 정렬을 위해 비교 결과를 반전
-                return entry2.getValue()[0].compareTo(entry1.getValue()[0]);
-            }
-        });
-        
-        
-        ArrayList<Integer> arrayList =new ArrayList<>();
-        
-        for (Map.Entry<String, Integer[]> entry : list) {
-            Integer[] array= entry.getValue();
-            int min=4;
-            int max=2; //먼저 출력할 인덱스 
+        // 장르별 곡 정보를 업데이트
+        for (int i = 0; i < genres.length; i++) {
+            String genre = genres[i];
+            int playCount = plays[i];
             
-            if(array[3]==0 ){ //하나의 곡만 들어있을 때
-                arrayList.add(array[max]);
-                continue;
-            }
+            genreMap.putIfAbsent(genre, new GenreInfo());
+            GenreInfo info = genreMap.get(genre);
             
-            if(array[1].equals(array[3])){ //플레이회수같다면 
-                if(array[2]>array[4]){
-                    min=2; max=4;
-                }
-            }
-            else if(array[1]<array[3]){ // arary3이 더 크면
-                min=2; max=4;
-            }
-            arrayList.add(array[max]);
-            arrayList.add(array[min]);
+            info.totalPlayCount += playCount;
+            info.addSong(playCount, i);
         }
         
-        answer=new int[arrayList.size()];
-        int size=0;
-        for(int temp:arrayList){
-            answer[size++]=temp;
+        // 장르별 총 재생수에 따라 내림차순 정렬
+        List<Map.Entry<String, GenreInfo>> genreList = new ArrayList<>(genreMap.entrySet());
+        genreList.sort((e1, e2) -> Integer.compare(e2.getValue().totalPlayCount, e1.getValue().totalPlayCount));
+        
+        // 결과를 저장할 리스트
+        List<Integer> result = new ArrayList<>();
+        
+        for (Map.Entry<String, GenreInfo> entry : genreList) {
+            GenreInfo info = entry.getValue();
+            result.addAll(info.getTopTwoSongs());
         }
         
-        
-        return answer;
+        // 결과 리스트를 배열로 변환
+        return result.stream().mapToInt(Integer::intValue).toArray();
     }
-
+    
+    // 장르 정보를 저장할 클래스
+    private static class GenreInfo {
+        int totalPlayCount = 0;
+        int[] songPlayCounts = new int[2];
+        int[] songIndices = new int[2];
+        
+        void addSong(int playCount, int index) {
+            if (songPlayCounts[0] < playCount) {
+                songPlayCounts[1] = songPlayCounts[0];
+                songIndices[1] = songIndices[0];
+                songPlayCounts[0] = playCount;
+                songIndices[0] = index;
+            } else if (songPlayCounts[1] < playCount) {
+                songPlayCounts[1] = playCount;
+                songIndices[1] = index;
+            }
+        }
+        
+        List<Integer> getTopTwoSongs() {
+            List<Integer> topSongs = new ArrayList<>();
+            if (songPlayCounts[0] > 0) topSongs.add(songIndices[0]);
+            if (songPlayCounts[1] > 0) topSongs.add(songIndices[1]);
+            return topSongs;
+        }
+    }
 }
