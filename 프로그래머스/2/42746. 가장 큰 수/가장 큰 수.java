d281308685d61689869301d08cb08f9c7885c3ca
hyojKim2 <101076275+hyojKim2@users.noreply.github.com>
2024-08-01 19:39:24 +0900

    [level 2] Title: 가장 큰 수, Time: 2985.24 ms, Memory: 392 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,38 +1,22 @@
-import java.util.PriorityQueue;
-import java.util.Collections;
 import java.util.*;
 
 class Solution {
-    public String solution(int[] numbers) {
-        String answer = "";
-         String[] strNumbers = new String[numbers.length];
-        for (int i = 0; i < numbers.length; i++) {
-            strNumbers[i] = String.valueOf(numbers[i]);
-        }
+    public String solution(int[] numbers)
+    {
+        String answer="";
+        String[] nums=new String[numbers.length];
 
-        // 문자열을 비교하여 더 큰 순서로 정렬
-        Arrays.sort(strNumbers, new Comparator<String>() {
-            @Override
-            public int compare(String o1, String o2) {
-                // 두 문자열을 합쳐서 비교
-                String s1 = o1 + o2;
-                String s2 = o2 + o1;
-                // 내림차순으로 정렬
-                return s2.compareTo(s1);
-            }
+        for(int i=0;i<numbers.length;i++)
+            nums[i]=String.valueOf(numbers[i]);
+
+        Arrays.sort(nums , (o1, o2)->{
+            return (o2+o1).compareTo(o1+o2); //
         });
 
-        // 정렬된 문자열을 이어붙여서 결과 생성
-        StringBuilder sb = new StringBuilder();
-        for (String str : strNumbers) {
-            sb.append(str);
-        }
+        for(String num:nums)
+            answer+=num;
 
-        // 배열이 모두 0으로 이루어진 경우 예외 처리
-        if (sb.charAt(0) == '0') {
-            return "0";
-        }
-
-        return sb.toString();
+        return (answer.charAt(0)=='0')? "0" : answer; //입력이 모두 0인 경우 처리
     }
+
 }
